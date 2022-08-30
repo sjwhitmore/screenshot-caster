951b67b27122d986d285fac9ff42385667eb0f8c
Samantha Whitmore <whitmore.samantha@gmail.com>
2022-08-30 15:40:33 -0700

    update homepage & ocr to include worker startup

--- pages/api/ocr.js
+++ pages/api/ocr.js
@@ -7,6 +7,9 @@ export default async function ocr(req, res) {
   const imgWorker = createWorker({
     logger: m => console.log(m)
   })
+  await worker.load()
+  await worker.loadLanguage('eng')
+  await worker.initialize('eng')
   const imgConfidenceLevel = 70
   if (cast.body.data.text.includes(imgurUrl)) {
     let image = imgurUrl + cast.body.data.text.split(imgurUrl)[1]
@@ -16,15 +19,17 @@ export default async function ocr(req, res) {
       // TODO do something with data.data.text (store in a column? different table?)
       // Different table is a good option b/c you could check if translation exists already & avoid re-indexing if so
       console.log(data.data.text);
-      return {"text": data.data.text}
+      res = {"text": data.data.text}
     } else {
       // just for debugging
       console.log('low confidence')
       console.log(data.data.text);
-      return {"error": "image unable to be translated"}
+      res = {"error": "image unable to be translated"}
     }
   } else {
     console.log("no image found")
-    return {"error": "no image found"}
+    res = {"error": "no image found"}
   }
+  await worker.terminate()
+  return res
 }
